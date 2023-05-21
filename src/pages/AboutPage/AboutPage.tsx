f751ce359b8c294abf7ce3b9ac5018dccf578393
cool29 <cool29@yandex.ru>
2023-05-21 11:41:34 +0500

    добавил контент на страницу о себе

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,5 +4,15 @@ import styles from './AboutPage.module.scss';
 interface AboutPageProps {}
 
 export const AboutPage: FC<AboutPageProps> = () => {
-  return <div className={styles.wrap}>AboutPage</div>;
+  return (
+    <div className={styles.wrap}>
+      <h2>Сведения об авторе проекта</h2>
+      <p>Меня зовут Василий. Я занимаюсь фронтент разработкой.</p>
+      <p>Основной стэк: JavaScript/TypeScript, react, redux-toolkit.</p>
+      <p>
+        Занимаюсь разработкой несколько лет, в том числе и с использованием
+        других технологий и за пределами фронтенда.
+      </p>
+    </div>
+  );
 };
